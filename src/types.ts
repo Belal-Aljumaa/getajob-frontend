e8713b941a8fb68b24d69dafd80378c6477ed2ad
Belal-Aljumaa <bilal.joma90@gmail.com>
2022-12-08 03:40:54 +0100

    new update

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type Job = {
+  skills: any;
+  id: number;
+  title: string;
+  company: string;
+  description: string;
+  url: string;
+  skillList: string;
+  todo: string;
+};
+
+export type Skill = {
+  idCode: string;
+  name: string;
+  url: string;
+  description: string;
+}
